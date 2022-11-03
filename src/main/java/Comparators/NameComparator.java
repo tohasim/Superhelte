33962d44c8fa0d63e0a51f616a2b74c8ee6e95f3
Adam <adam0399@stud.kea.dk>
2022-11-03 16:21:29 +0100

    NameComparator done

--- src/main/java/Comparators/NameComparator.java
+++ src/main/java/Comparators/NameComparator.java
@@ -1,4 +1,12 @@
 package Comparators;
 
-public class NameComparator {
+import MainClasses.Superhero;
+
+import java.util.Comparator;
+
+public class NameComparator implements Comparator<Superhero> {
+    public int compare(Superhero o1, Superhero o2){
+        return o1.getName().compareTo(o2.getName());
+    }
+
 }
