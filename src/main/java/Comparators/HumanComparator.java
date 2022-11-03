14d264f9b2351d0091c2ef34982a3b9c6754de39
sadekkk12 <sade0027@stud.kea.dk>
2022-11-03 16:25:02 +0100

    HumanComparator oprettet

--- src/main/java/Comparators/HumanComparator.java
+++ src/main/java/Comparators/HumanComparator.java
@@ -1,4 +1,14 @@
 package Comparators;
 
-public class HumanComparator {
+import FileAndDatabase.Database;
+import MainClasses.Superhero;
+
+import java.util.Comparator;
+
+public class HumanComparator implements Comparator<Superhero> {
+
+    @Override
+    public int compare(Superhero o1, Superhero o2) {
+        return o1.getName().compareTo(o2.getName());
+    }
 }
