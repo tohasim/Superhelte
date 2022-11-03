9035c4247fbeeea9fa3a60d2e445d5e7012b35c1
HostilePlayer <mart5e86@stud.kea.dk>
2022-11-03 19:02:31 +0100

    StrengthComparator

--- src/main/java/Comparators/StrengthComparator.java
+++ src/main/java/Comparators/StrengthComparator.java
@@ -1,4 +1,13 @@
 package Comparators;
 
-public class StrengthComparator {
+import MainClasses.Superhero;
+
+import java.util.Comparator;
+
+public class StrengthComparator implements Comparator<Superhero> {
+
+    @Override
+    public int compare(Superhero o1, Superhero o2) {
+        return o1.getStrength() - o2.getStrength();
+    }
 }
