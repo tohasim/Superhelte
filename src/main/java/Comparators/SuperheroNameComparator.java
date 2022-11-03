64e2f52b76e553854290f6d10d714a9b3dcc1cbf
AmingYee <wenm0007@stud.kea.dk>
2022-11-03 20:19:42 +0100

    Supename

--- src/main/java/Comparators/SuperheroNameComparator.java
+++ src/main/java/Comparators/SuperheroNameComparator.java
@@ -1,4 +1,12 @@
 package Comparators;
 
-public class SuperheroNameComparator {
+import MainClasses.Superhero;
+
+import java.util.Comparator;
+
+public class SuperheroNameComparator implements Comparator<Superhero> {
+    @Override
+    public int compare(Superhero o1, Superhero o2){
+        return o1.getSuperheroName().compareTo(o2.getSuperheroName());
+    }
 }
