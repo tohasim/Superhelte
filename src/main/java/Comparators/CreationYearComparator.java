f430f0f458d1463532a9d55432451cd7d34b4ecd
Tohasim <toreh.simonsen@live.dk>
2022-11-03 16:19:03 +0100

    CreationYearComparator finished

--- src/main/java/Comparators/CreationYearComparator.java
+++ src/main/java/Comparators/CreationYearComparator.java
@@ -1,4 +1,12 @@
 package Comparators;
 
-public class CreationYearComparator {
+import MainClasses.Superhero;
+
+import java.util.Comparator;
+
+public class CreationYearComparator implements Comparator<Superhero> {
+    @Override
+    public int compare(Superhero o1, Superhero o2) {
+        return o1.getCreationYear() - o2.getCreationYear();
+    }
 }
