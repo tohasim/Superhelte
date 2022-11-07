ddce7b4d28dbcd83c94e31ab986297657d8f8b61
HostilePlayer <mart5e86@stud.kea.dk>
2022-11-07 14:23:21 +0100

    lavede music bedre, kan nu næmere finde filen

--- src/main/java/Music.java
+++ src/main/java/Music.java
@@ -6,7 +6,7 @@ public class Music {
 
     public void loadMusic(){
         //make sure file name and location is correct
-        File backgroundMusic = new File("C:\\Users\\Hostile\\IdeaProjects\\Adventure\\Sound\\GameMusic.wav");
+        File backgroundMusic = new File("Sound\\GameMusic.wav");
         playMusic(backgroundMusic);
     }
 
